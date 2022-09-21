c21b987bef9504acd170dbb9067a28ed6d1e9c3b
Alex <Alex@ALEX-PC>
2022-09-21 17:29:17 +0300

    Добавьте файлы проекта.

---
+++ VS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("My first repo");
+            Console.ReadKey();
+        }
+    }
+}
